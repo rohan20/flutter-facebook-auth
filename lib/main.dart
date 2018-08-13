5940a8a2b583c7e886e4bc3e491cd41948455a6d
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-08-13 12:12:32 +0530

    setup basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,24 @@
-void main(){
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(LoginPage());
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Facebook Login"),
+        ),
+        body: Container(),
+      ),
+    );
+  }
 }
